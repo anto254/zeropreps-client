6843435c7bfdcc874de101032d189df302046bc8
anto254 <jenga.anto@gmail.com>
2024-06-21 22:40:10 +0300

    fixed

--- src/pages/ChatWithUs.jsx
+++ src/pages/ChatWithUs.jsx
@@ -112,7 +112,7 @@ function ChatWithUs() {
                         </button>
                         <div className="bg-red-500 py-3 text-light rounded-t-md px-2">
                             <h1 className="font-bold text-lg text-light">Welcome to Zeropreps</h1>
-                            <h1 className='text-xs font-bold md:font-semibold' >If we can't do it, we'll say it. If we can, we'll make it happen. ProctorU Exams ONLY!</h1>
+                            <h1 className='font-bold md:font-semibold' >If we can't do it, we'll say it. If we can, we'll make it happen. ProctorU Exams ONLY!</h1>
                         </div>
 
                         {/* Your chat content goes here */}
