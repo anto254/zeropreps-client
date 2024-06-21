88667ecaaf73229aec2c5b6f0b7855340f6b864e
anto254 <jenga.anto@gmail.com>
2024-06-21 22:57:21 +0300

    fixed cta

--- src/components/CtaBanner.jsx
+++ src/components/CtaBanner.jsx
@@ -7,10 +7,10 @@ const CtaBanner = () => {
     const { auth, setAuth } = useAuth();
 
     return (
-        <div className="bg-light py-6 my-5 px-3 rounded-lg">
+        <div className="bg-blue-500 data-nosnippet py-6 my-5 px-3 rounded-lg">
             <div className="container mx-auto flex items-center justify-between">
                 {/* <p className="text-white text-lg">Need help with your ProctorU exam? Chat with us!</p> */}
-                <p className="text-dark text-lg basis-10/12">Finding it hard to crack ProctorU exams? Hire Us! </p>
+                <p className="text-light text-lg md:text-xl font-bold basis-10/12">Finding it hard to crack ProctorU exams? Hire Us! </p>
 
                 <button className="bg-red-500 basis-2/12 flex justify-center items-center hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
                     onClick={() => {
